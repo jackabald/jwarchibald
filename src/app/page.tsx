7cc3c72939731321c47941be14ff55e73d57ebcc
Jack Archibald <jwarchibald@wisc.edu>
2025-06-08 19:28:56 -0500

    fix raw apostrohpes and <a> tags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { Hero } from '@/components/Hero'
 
 export default function Page() {
@@ -8,26 +9,26 @@ export default function Page() {
       <section className="prose prose-lg mb-12">
         <p>Hi there!</p>
         <p>
-          I'm Jack Archibald.  I'm currently a software engineering intern at GE HealthCare, where I work with 
-          backend microservices for medical resonance imaging (MRI) platforms using Spring Boot, Docker, and Jenkins.
+          I&apos;m Jack Archibald.  I&apos;m currently a software engineering intern at GE HealthCare, where I work with 
+          backend microservices for medical resonance imaging (MRI) platforms using Spring Boot, Gradle, and Jenkins.
         </p>
         <p>
-          Previously, I was a Field Technician at UW-Madison's Division of IT, where I delivered campus-wide tech support, 
+          Previously, I was a Field Technician at UW-Madison&apos;s Division of IT, where I delivered campus-wide tech support, 
           imaged and secured workstations, and resolved technical issues through remote system access and diagnostics.
         </p>
       </section>
 
-      <h2 className="text-xl font-semibold mb-4">Here are some things I've done:</h2>
+      <h2 className="text-xl font-semibold mb-4">Here are some things I&apos;ve done:</h2>
       <ul className="space-y-2">
         <li>
-          <a href="/" className="text-primary hover:underline">
+          <Link href="/" className="text-primary hover:underline">
             Placeholder<span className="text-dark/60"> — May 17, 2024</span>
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/" className="text-primary hover:underline">
+          <Link href="/" className="text-primary hover:underline">
             Placeholder<span className="text-dark/60"> — May 31, 2023</span>
-          </a>
+          </Link>
         </li>
       </ul>
     </>
